c8736f56bc0643cb31d05174b3b1eb32792ca6bf
aloepker <aloepker@gmail.com>
2023-09-09 18:29:33 -0500

    Got fork() and exec1() to work correctly on oss.c. Updated the readme also.

--- oss.c
+++ oss.c
@@ -1,8 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+void help(){
+printf("The options for the program are:\n");
+printf("-n <number>   this sets the number of processes to launch\n");
+printf("-s <number>   this sets how many processes can run at once\n");
+printf("-t <number>   this sets the number of loops the worker(s) perform\n");
+
+}
 
 int main(int argc, char** argv){
-printf("Hello world from OSS.c\n");
+int option;
+int numWorkers = 0;
+int workerLimit = 0;
+int iter = 0;
+
+while((option = getopt(argc, argv, "hn:s:t:")) != -1){
+	switch(option){
+		case 'h':
+			help();
+			return EXIT_SUCCESS;
+		case 'n':
+			numWorkers = atoi(optarg);
+			break;
+		case 's':
+			workerLimit = atoi(optarg);
+			break;
+		case 't':
+			iter = atoi(optarg);
+			break;
+		case '?':
+			if ((optopt = 'c'))
+				printf("Option %c requires an argument\n", optopt);
+			else if (isprint(optopt))
+				printf("Unknown Character '\\x%x'.\n", optopt);
+			return 1;
+		default:
+			help();
+			return EXIT_SUCCESS;
+	}
+
+
+}
+
+printf("Number of workers Selected:%d\nNumber of Workers at a time:%d\nNumber of loops for each Worker:%d\n", numWorkers, workerLimit, iter);
+printf("Now we are cooking!\n");
+
+pid_t childPid = fork();
+if (childPid == 0) {
+
+char argString[20];
+sprintf(argString, "%d", iter);
+
+execl("./worker","./worker", argString, NULL);
+
+} else {
+int stat;
+wait(&stat);
+printf("Wait opperation compleated successfully!\n");
+}
+
+
 }
 
 // this program will launch a certian number of worker processes with certian parameters.
--- worker.c
+++ worker.c
@@ -8,7 +8,7 @@ printf("Starting loop\n");
 int i;
 int j = atoi(argv[1]);
 for (i=0;i<j;i++){
-printf("Worker PID:%d PPID:%d Iteration:%d Before Sleeping\n",getpid(), getppid(), i+1);
+printf("Worker PID:%d PPID:%d Iteration: %d Before Sleeping\n",getpid(), getppid(), i+1);
 sleep(1);
 printf("Worker PID:%d PPID:%d Iteration: %d After Sleeping\n",getpid(), getppid(), i+1);
 }
