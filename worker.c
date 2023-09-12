7ed03a6f03ba5e7acca7a0b6b06752d22604aba3
aloepker <aloepker@gmail.com>
2023-09-11 20:46:02 -0500

    Last commit! Code layout was organized in oss.c and worker.c. README was revised to specification. Project1 is ready to turn in!

--- worker.c
+++ worker.c
@@ -3,19 +3,16 @@
 #include <unistd.h>
 
 int main(int argc, char** argv){
-if(argc>1){
-printf("Starting loop\n");
-int i;
-int j = atoi(argv[1]);
-for (i=0;i<j;i++){
-printf("Worker PID:%d PPID:%d Iteration: %d Before Sleeping\n",getpid(), getppid(), i+1);
-sleep(1);
-printf("Worker PID:%d PPID:%d Iteration: %d After Sleeping\n",getpid(), getppid(), i+1);
+	if(argc>1){
+		printf("Starting loop\n");
+		int i;
+		int j = atoi(argv[1]);
+		for (i=0;i<j;i++){
+			printf("Worker PID:%d PPID:%d Iteration: %d Before Sleeping\n",getpid(), getppid(), i+1);
+			sleep(1);
+			printf("Worker PID:%d PPID:%d Iteration: %d After Sleeping\n",getpid(), getppid(), i+1);
+		}
+	} else {
+		printf("incorrect number of arguments\n");
+	}
 }
-}else{
-printf("incorrect number of arguments\n");
-}
-}
-
-
-//each pass through the loop: output the pid, parents pid (ppid),& current iteration of the loop
